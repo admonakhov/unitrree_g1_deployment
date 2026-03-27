06d09d13ec380eeb533b332a1a56cc0f1ebd9aec
Anton Monakhov <a.monahov@tb-drone.ru>
2026-03-27 11:22:18 +0300

    audio added

--- include/FSM/FSMState.h
+++ include/FSM/FSMState.h
@@ -8,6 +8,8 @@
 #include "isaaclab/devices/keyboard/keyboard.h"
 #include "isaaclab/devices/gamepad/gamepad.h"
 #include "unitree_joystick_dsl.hpp"
+#include <spdlog/spdlog.h>
+#include <chrono>
 
 class FSMState : public BaseState
 {
@@ -38,9 +40,19 @@ public:
                 unitree::common::dsl::Parser p(condition);
                 auto ast = p.Parse();
                 auto func = unitree::common::dsl::Compile(*ast);
+                auto wrapped = [func, condition, fsm_id, state_string]()->bool{
+                    bool res = func(FSMState::lowstate->joystick);
+                    if (res) {
+                        spdlog::info("FSM: Condition '{}' triggered in State_{} -> State_{}",
+                                     condition,
+                                     state_string,
+                                     FSMStringMap.left.at(fsm_id));
+                    }
+                    return res;
+                };
                 registered_checks.emplace_back(
                     std::make_pair(
-                        [func]()->bool{ return func(FSMState::lowstate->joystick); },
+                        wrapped,
                         fsm_id
                     )
                 );
@@ -67,6 +79,18 @@ public:
             }
         }
         if(keyboard) keyboard->update();
+
+        // Debug LT/down inputs while in Velocity state (throttled)
+        if (getStateString() == "Velocity") {
+            static auto last_log = std::chrono::steady_clock::now();
+            auto now = std::chrono::steady_clock::now();
+            if (now - last_log > std::chrono::milliseconds(500)) {
+                last_log = now;
+                const auto & j = lowstate->joystick;
+                spdlog::info("Velocity input: LT pressed={} t={:.2f}, down pressed={} on_pressed={}",
+                             j.LT.pressed, j.LT.pressed_time, j.down.pressed, j.down.on_pressed);
+            }
+        }
     }
 
     void post_run()
