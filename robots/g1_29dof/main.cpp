ce6d3e05f00eae7410e678727797b88aa1241c76
Anton Monakhov <a.monahov@tb-drone.ru>
2026-03-27 10:26:45 +0300

    gamepad_controller

--- robots/g1_29dof/main.cpp
+++ robots/g1_29dof/main.cpp
@@ -7,6 +7,7 @@
 std::unique_ptr<LowCmd_t> FSMState::lowcmd = nullptr;
 std::shared_ptr<LowState_t> FSMState::lowstate = nullptr;
 std::shared_ptr<Keyboard> FSMState::keyboard = std::make_shared<Keyboard>();
+std::shared_ptr<isaaclab::Gamepad> FSMState::gamepad = nullptr;
 
 void init_fsm_state()
 {
@@ -23,6 +24,7 @@ void init_fsm_state()
     spdlog::info("Waiting for connection to robot...");
     FSMState::lowstate->wait_for_connection();
     spdlog::info("Connected to robot.");
+    FSMState::gamepad = std::make_shared<isaaclab::Gamepad>();
 }
 
 int main(int argc, char** argv)
